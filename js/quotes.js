c57916880070a4bf63f5b72f42aa1000d464cd77
letsummer <letsummerparty@gmail.com>
2023-02-06 00:39:49 +0900

    added quotes

--- js/quotes.js
+++ js/quotes.js
@@ -23,6 +23,34 @@ const quotes = [
         quote: "끝날 때까지 끝난 게 아니다",
         author: "베이브 루스",
     },
+    {
+        quote: "우승 말고는 딱히 하고 싶지도 않습니다.",
+        author: "이정후",
+    },
+    {
+        quote: "뭐야...뭔소리야 이게?",
+        author: "최원태",
+    },
+    {
+        quote: "키움 빼고는 다 별로야",
+        author: "오주원",
+    },
+    {
+        quote: "얘는 진짜 키움을 사랑해",
+        author: "심수창",
+    },
+    {
+        quote: "팬분들 제가 초밥 사드리고 싶네요",
+        author: "최원태",
+    },
+    {
+        quote: "엄마가 잘생겼다는 사람 조심하래요",
+        author: "이승호",
+    },
+    {
+        quote: "사실 미리 하면 과제라는게 그렇게 어려운게 아니거든요?",
+        author: "고해주",
+    },
 ];
 
 const quote = document.querySelector(".quote span:first-child");
