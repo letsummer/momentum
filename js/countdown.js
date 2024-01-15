25c5ff74b6a8ac70510f1e295eb8c62e1eab49f9
letsummer <letsummerparty@gmail.com>
2024-01-15 16:27:39 +0900

    개막일 음수, 양수 수정

--- js/countdown.js
+++ js/countdown.js
@@ -2,11 +2,14 @@ const clockTitle = document.querySelector("#countdown");
 
 function getClock() {
   const date = new Date();
-  const playBall = new Date("2023-04-01 0:00:00");
+  const playBall = new Date("2024-03-23 0:00:00");
   const dday = Math.round((playBall - date) / (1000 * 60 * 60 * 24));
   const days = String(dday).padStart(2, "0");
 
-  clockTitle.innerText = `${days}`;
+  if(days>0)
+    clockTitle.innerText = `-${days}`;
+  else
+    clockTitle.innerText = `+${days}`;
 //   console.log(days);
 }
 
